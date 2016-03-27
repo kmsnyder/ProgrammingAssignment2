db211ceab77f86f186a4032f7aebda517a07bb70
kmsnyder <snyder.kris@gmail.com>
2016-03-27 15:41:14 -0700

    The changes made to the file were needed for submission of this homework assignment. This script has been updated to include the ability to cache a matrix and its identify for quicker access.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The following function takes a matrix as input and 
+## sets up in memory cache to store a solve value and set a solve value
+## into the matrix. This function doesn't not test if the matrix is a square matrix,
+## but it assumes that the user inputs a square matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+      x <<- y
+      m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) s <<- solve
+  getSolve <- function() s
+  list(set = set, get = get,
+       setSolve = setSolve, getSolve = getSolve)
 }
 
-
-## Write a short comment describing this function
+## The following function checks to see if the matrix has been cached
+## if so returns the value otherwise solves the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    s <- x$getSolve()
+    if(!is.null(s)){
+      message("getting data from cache")
+      return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setSolve(s)
+    s
 }
